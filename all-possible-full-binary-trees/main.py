4ba5d636e9fe136fc27c93091c5e2af864a3d8df
scirelli <scirelli+git@gmail.com>
2021-11-05 23:04:49 -0400

    Updated based on problem

--- all-possible-full-binary-trees/main.py
+++ all-possible-full-binary-trees/main.py
@@ -1,34 +1,30 @@
+#!/usr/bin/env python3
 """
-n = 5
-[n1,n2,n3,n4,n5]
+Given an integer n, return a list of all possible full binary trees with n nodes. Each node of each tree in the answer must have Node.val == 0.
+Each element of the answer is the root node of one possible tree. You may return the final list of trees in any order.
+A full binary tree is a binary tree where each node has exactly 0 or 2 children.
 
-      (n1)    [n2,n3,n4,n5]
+Example 1:
+    Input: n = 7
+    Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
 
+Example 2:
+    Input: n = 3
+    Output: [[0,0,0]]
 
-      (n1)    [n4,n5]
-      /  \
-   (n2)  (n3)
-
-
-
-      (n1)   []
-      /  \
-   (n2)  (n3)
-   /  \
-(n4)  (n5)
-
-
-      (n1)    [n4,n5]
-      /  \
-   (n2)  (n3)
-
-
-      (n1)   []
-      /  \
-   (n2)  (n3)
-         /. \
-      (n4)  (n5)
-
-
-      (n1)   [n2,n3,n4,n5]
+Constraints:
+    1 <= n <= 20
 """
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val: int = 0, left:Optional[TreeNode] = None, right: Optional[TreeNode] = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        pass
